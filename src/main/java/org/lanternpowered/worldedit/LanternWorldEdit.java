84ed57429fc4f8f9950f5c298ee4dbd995a737f8
Seppe Volkaerts <seppevolkaerts@hotmail.com>
2017-01-29 21:21:10 +0100

    Fix headers.

--- src/main/java/org/lanternpowered/worldedit/LanternWorldEdit.java
+++ src/main/java/org/lanternpowered/worldedit/LanternWorldEdit.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of LanternWorldEdit, licensed under the MIT License (MIT).
  *
- * Copyright (c) LanternPowered https://www.lanternpowered.org
+ * Copyright (c) LanternPowered <https://www.lanternpowered.org>
  * Copyright (c) contributors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
